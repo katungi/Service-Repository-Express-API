54199a2f176ba9689e8dbad935b77f18076fbab5
Daniel Dennis <dankatdennis@gmail.com>
2021-07-06 15:20:04 +0300

    - move server setup to the application abstraction

---
+++ src/application.ts
@@ -0,0 +1,16 @@
+export class App {
+  async setup(){
+    const container = new Container()
+    const server = new InversifyExpressServer(container)
+
+    server.setConfig((app) => {
+      app.use(express.json())
+    })
+
+    const app = server.build()
+
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running on port ${process.env.PORT}`)
+    })
+  }
+}
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -20,19 +20,6 @@ export async function bootstrap() {
   });
 
   //  mongoose.model('Subscriber', subscribersModel)
-
-  const container = new Container()
-  const server = new InversifyExpressServer(container)
-
-  server.setConfig((app) => {
-    app.use(express.json())
-  })
-
-  const app = server.build()
-
-  app.listen(process.env.PORT, () => {
-    console.log(`server is running on port ${process.env.PORT}`)
-  })
   return {}
 }
 

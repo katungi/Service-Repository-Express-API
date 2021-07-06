e591005042745fbc192ceb667bc563a4eaf8b88f
Daniel Dennis <dankatdennis@gmail.com>
2021-07-06 14:24:59 +0300

    - init and basic setup

--- src/bootstrap.spec.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrap } from '@/bootstrap'
-
-describe('bootstrap should', () => {
-  it('return an empty object', () => {
-    expect(bootstrap()).toStrictEqual({})
-  })
-})
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,6 +1,13 @@
 import 'dotenv/config'
+import express from 'express'
+import { Container } from 'inversify'
+import { InversifyExpressServer } from 'inversify-express-utils'
 
-export function bootstrap() {
+export async function bootstrap() {
+  const container = new Container()
+  const server = new InversifyExpressServer(container)
+
+  server.setConfig((app)=>)
   return {}
 }
 
